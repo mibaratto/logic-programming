22f8f85262012da5cf8ccba5595e3491ce88b24a
Michelle Lang Baratto <mi.baratto@gmail.com>
2025-02-28 09:30:46 +0100

    aula 3 parte 3

--- exercisesClass03.py
+++ exercisesClass03.py
@@ -15,25 +15,42 @@ import math
 
 # ///////////////////////////////////////////////
 
-horario_hora_inicio = int(input("Informe a hora de inicio: "))
-horario_minutos_inicio = int(input("Informe os minutos de inicio: "))
-horario_hora_fim = int(input("Informe a hora de fim: "))
-horario_minutos_fim = int(input("Informe os minutos de fim: "))
+# horario_hora_inicio = int(input("Informe a hora de inicio: "))
+# horario_minutos_inicio = int(input("Informe os minutos de inicio: "))
+# horario_hora_fim = int(input("Informe a hora de fim: "))
+# horario_minutos_fim = int(input("Informe os minutos de fim: "))
 
 
-hora_inicio_em_minutos = (horario_hora_inicio * 60 ) + horario_minutos_inicio
-hora_fim_em_minutos = (horario_hora_fim * 60) + horario_minutos_fim
+# hora_inicio_em_minutos = (horario_hora_inicio * 60 ) + horario_minutos_inicio
+# hora_fim_em_minutos = (horario_hora_fim * 60) + horario_minutos_fim
 
-if horario_hora_inicio > horario_hora_fim :
-  minutos_total_dia = 24 * 60
-  tempo_total_jogo_minutos = (minutos_total_dia - hora_inicio_em_minutos) + hora_fim_em_minutos
-  tempo_jogo_horas = tempo_total_jogo_minutos // 60
-  tempo_parcial_minutos = tempo_total_jogo_minutos%60
+# if horario_hora_inicio > horario_hora_fim :
+#   minutos_total_dia = 24 * 60
+#   tempo_total_jogo_minutos = (minutos_total_dia - hora_inicio_em_minutos) + hora_fim_em_minutos
 
-else:
-  tempo_total_jogo_minutos = hora_fim_em_minutos - hora_inicio_em_minutos
-  tempo_jogo_horas = tempo_total_jogo_minutos // 60
-  tempo_parcial_minutos = tempo_total_jogo_minutos%60
 
-print(tempo_jogo_horas, tempo_parcial_minutos)
+# else:
+#   tempo_total_jogo_minutos = hora_fim_em_minutos - hora_inicio_em_minutos
+
+# tempo_jogo_horas = tempo_total_jogo_minutos // 60
+# tempo_parcial_minutos = tempo_total_jogo_minutos%60
+
+# print(tempo_jogo_horas, tempo_parcial_minutos)
+
+# ///////////////////////////////////////////
+
+numero = int(input("digite um numero de 4 digitos: "))
+numero >= 1000 and numero <=  9999
+
+digito_4 = numero % 10
+digito_3 = (numero // 10) % 10
+digito_2 = (numero // 100 ) % 10
+digito_1 = (numero // 1000) % 10
+
+if digito_1 == digito_4 and digito_2 == digito_3 :
+  print ("numero é capicua")
+else: print("numero não é capicua")
+
+print(digito_1, digito_2, digito_3, digito_4)
+print(digito_4, digito_3, digito_2, digito_1)
 
